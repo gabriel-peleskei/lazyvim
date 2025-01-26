cb8aa9eac896fa77fb77933021cde3fda1d13fa6
Gabriel Peleskei <gabriel.peleskei@gmail.com>
2025-01-26 20:48:02 +0100

    tabs removed, neotree as transparent popup, mini.ai added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,21 +5,21 @@
 local map = vim.keymap.set
 
 map("n", "\\", function()
-  if vim.bo.filetype == "neo-tree" then
-    require("neo-tree.command").execute({ action = "close" })
-    return
-  end
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.bo[buf].filetype == "neo-tree" then
-      for _, win in ipairs(vim.api.nvim_list_wins()) do
-        if vim.api.nvim_win_get_buf(win) == buf then
-          vim.api.nvim_set_current_win(win)
-          return
-        end
-      end
-    end
-  end
-  require("neo-tree.command").execute({ focus = true, position = "current" })
+  -- if vim.bo.filetype == "neo-tree" then
+  --   require("neo-tree.command").execute({ action = "close" })
+  --   return
+  -- end
+  -- for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+  --   if vim.bo[buf].filetype == "neo-tree" then
+  --     for _, win in ipairs(vim.api.nvim_list_wins()) do
+  --       if vim.api.nvim_win_get_buf(win) == buf then
+  --         vim.api.nvim_set_current_win(win)
+  --         return
+  --       end
+  --     end
+  --   end
+  -- end
+  require("neo-tree.command").execute({ position = "float" })
 end, { desc = "Toggle Neotree" })
 map("n", "<c-left>", "<cmd>winc h<cr>", { desc = "Move to left window", remap = true })
 map("n", "<c-right>", "<cmd>winc l<cr>", { desc = "Move to left window", remap = true })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,31 +8,53 @@ return {
       "MunifTanjim/nui.nvim",
     },
     cmd = "Neotree",
-    opts = {
-      filesystem = {
-        hijack_netrw_behavior = "open_current",
-        window = {
-          mappings = {
-            ["\\"] = "close_window",
+    opts = function(_)
+      vim.api.nvim_set_hl(0, "NeoTreeNormal", { background = "NONE" })
+      -- vim.cmd("highlight NeoTreeNormal guibg=NONE ctermbg=NONE")
+      return {
+        filesystem = {
+          hijack_netrw_behavior = "disabled",
+          window = {
+            mappings = {
+              ["\\"] = "close_window",
+            },
+            popup = {
+              position = {
+                col = "50%",
+                row = "50%",
+              },
+              size = function()
+                local cols = vim.o.columns
+                local rows = vim.o.lines
+                return {
+                  width = math.floor(cols * 0.99),
+                  height = math.floor(rows * 0.99),
+                }
+              end,
+              border = "rounded",
+              win_options = {
+                winblend = 100,
+              },
+            },
+          },
+          -- bind_to_cwd = false,
+          use_libuv_file_watcher = true,
+        },
+        event_handlers = {
+          {
+            event = "file_open_request",
+            handler = function()
+              require("neo-tree.command").execute({ action = "close" })
+            end,
+          },
+          {
+            event = "file_opened",
+            handler = function()
+              require("neo-tree").close_all()
+            end,
           },
         },
-        -- bind_to_cwd = false,
-        use_libuv_file_watcher = true,
-      },
-      event_handlers = {
-        {
-          event = "file_open_request",
-          handler = function()
-            require("neo-tree.command").execute({ action = "close" })
-          end,
-        },
-        {
-          event = "file_opened",
-          handler = function()
-            require("neo-tree").close_all()
-          end,
-        },
-      },
-    },
+      }
+    end,
   },
 }
