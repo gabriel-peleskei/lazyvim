a398e41009468fb3a9dd57b90195c8e663aec8e6
Gabriel Peleskei <gabriel.peleskei@gmail.com>
2025-01-30 22:01:35 +0100

    keymap updatec

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,21 +4,15 @@
 --
 local map = vim.keymap.set
 
+local diag_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity or vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+
 map("n", "\\", function()
-  -- if vim.bo.filetype == "neo-tree" then
-  --   require("neo-tree.command").execute({ action = "close" })
-  --   return
-  -- end
-  -- for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-  --   if vim.bo[buf].filetype == "neo-tree" then
-  --     for _, win in ipairs(vim.api.nvim_list_wins()) do
-  --       if vim.api.nvim_win_get_buf(win) == buf then
-  --         vim.api.nvim_set_current_win(win)
-  --         return
-  --       end
-  --     end
-  --   end
-  -- end
   require("neo-tree.command").execute({ position = "float" })
 end, { desc = "Toggle Neotree" })
 map("n", "<c-left>", "<cmd>winc h<cr>", { desc = "Move to left window", remap = true })
@@ -27,3 +21,6 @@ map("n", "<c-up>", "<cmd>winc k<cr>", { desc = "Move to left window", remap = tr
 map("n", "<c-down>", "<cmd>winc j<cr>", { desc = "Move to left window", remap = true })
 --
 map("n", "<leader>q<space>", "<cmd>q<cr>", { desc = "Quit Buffer" })
+map("n", "<leader>ee", diag_goto(true, vim.diagnostic.severity.ERROR), { desc = "Next Error" })
+map("n", "<leader>en", diag_goto(true), { desc = "Next Diagnostic" })
+map("n", "<leader>ep", diag_goto(false), { desc = "Previous Diagnostic" })
